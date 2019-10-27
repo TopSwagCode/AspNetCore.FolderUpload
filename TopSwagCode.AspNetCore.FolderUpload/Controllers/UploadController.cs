7c857d6c0968955824a8d8b15950015a5ddafa79
Joshua Ryder <joshua.ryder@outlook.com>
2019-10-27 16:57:57 +0100

    Init

---
+++ TopSwagCode.AspNetCore.FolderUpload/Controllers/UploadController.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TopSwagCode.AspNetCore.FolderUpload.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/upload/{filename}/{fullpath}")]
+        public IActionResult Upload(string filename, string fullpath)
+        {
+            
+            var rootFolder = Directory.GetCurrentDirectory();
+
+            var uploadFolder = rootFolder + "\\uploads";
+
+            var filenameDecoded = System.Web.HttpUtility.UrlDecode(filename);
+            var fullpathDecoded = System.Web.HttpUtility.UrlDecode(fullpath);
+
+            var path = fullpathDecoded.Replace(filenameDecoded, "");
+
+            System.IO.Directory.CreateDirectory($"{uploadFolder}{path}");
+
+            using (Stream output = System.IO.File.OpenWrite(uploadFolder + fullpathDecoded))
+            using (Stream input = Request.BodyReader.AsStream())
+            {
+                input.CopyTo(output);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ TopSwagCode.AspNetCore.FolderUpload/wwwroot/js/app.js
@@ -0,0 +1,98 @@
+﻿// Drop handler function to get all files
+async function getAllFileEntries(dataTransferItemList) {
+    let fileEntries = [];
+    // Use BFS to traverse entire directory/file structure
+    let queue = [];
+    // Unfortunately dataTransferItemList is not iterable i.e. no forEach
+    for (let i = 0; i < dataTransferItemList.length; i++) {
+        queue.push(dataTransferItemList[i].webkitGetAsEntry());
+    }
+    while (queue.length > 0) {
+        let entry = queue.shift();
+        if (entry.isFile) {
+            fileEntries.push(entry);
+        } else if (entry.isDirectory) {
+            let reader = entry.createReader();
+            queue.push(...await readAllDirectoryEntries(reader));
+        }
+    }
+    return fileEntries;
+}
+
+// Get all the entries (files or sub-directories) in a directory by calling readEntries until it returns empty array
+async function readAllDirectoryEntries(directoryReader) {
+    let entries = [];
+    let readEntries = await readEntriesPromise(directoryReader);
+    while (readEntries.length > 0) {
+        entries.push(...readEntries);
+        readEntries = await readEntriesPromise(directoryReader);
+    }
+    return entries;
+}
+
+// Wrap readEntries in a promise to make working with readEntries easier
+async function readEntriesPromise(directoryReader) {
+    try {
+        return await new Promise((resolve, reject) => {
+            directoryReader.readEntries(resolve, reject);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+var elDrop = document.getElementById('dropzone');
+var elItems = document.getElementById('items');
+
+elDrop.addEventListener('dragover', function (event) {
+    event.preventDefault();
+    elItems.innerHTML = 0;
+});
+
+elDrop.addEventListener('drop', async function (event) {
+    event.preventDefault();
+    let items = await getAllFileEntries(event.dataTransfer.items);
+    elItems.innerHTML = items.length;
+
+    for (let i = 0; i < items.length; i++) {
+
+        if (items[i].isFile) {
+            ReadFileAndUpload(items[i], console.log, console.log)
+        }
+    }
+    
+});
+
+function ReadFileAndUpload(entry, successCallback, errorCallback) {
+    entry.file(function (file) {
+        let reader = new FileReader();
+
+        var li = document.createElement('li');
+        li.setAttribute("id", entry.fullPath);
+        li.innerHTML = entry.fullPath;
+
+        elItems.appendChild(li);
+
+        reader.onload = function () {
+
+            var url = '/upload/' + encodeURIComponent(entry.name) + '/' + encodeURIComponent(entry.fullPath); 
+
+            fetch(url, {
+                method: 'POST',
+                body: file
+            }).then(
+                // success => console.log(success)
+            ).catch(
+                error => console.log(error)
+            );
+
+            //successCallback(reader.result);
+        };
+
+        reader.onerror = function () {
+            errorCallback(reader.error);
+        }
+
+        reader.readAsText(file);
+    }, errorCallback);
+}
